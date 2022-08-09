92fbe2b74bce727326ec6f7d1e2d763982962a51
Jvribeirof <107144992+Jvribeirof@users.noreply.github.com>
2022-08-09 20:27:15 -0300

    att

---
+++ index/index.js
@@ -0,0 +1,10 @@
+//$(document).ready(function(){
+    const conteudo = [
+        {titulo:'olá mundo!(0)', texto:'fbklfglksfysbikgsbkfbsçfbyvlsvvsuo~sviçbuvrsbçs,'},
+        {titulo:'olá mundo!(1)', texto:'fbklfglksfysbikgsbkfbsçfbyvlsvvsuo~sviçbuvrsbçs,'},
+        {titulo:'olá mundo!(2)', texto:'fbklfglksfysbikgsbkfbsçfbyvlsvvsuo~sviçbuvrsbçs,'},
+        {titulo:'olá mundo!(3)', texto:'fbklfglksfysbikgsbkfbsçfbyvlsvvsuo~sviçbuvrsbçs,'}
+    ]
+
+    console.log(conteudo)
+//})
