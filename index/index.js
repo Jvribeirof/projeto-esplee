4a984600a73f72ee73b0829d98aeab8324b888b5
Jvribeirof <107144992+Jvribeirof@users.noreply.github.com>
2022-08-10 08:53:04 -0300

    att

--- index/index.js
+++ index/index.js
@@ -1,10 +1,15 @@
-//$(document).ready(function(){
+$(document).ready(function(){
     const conteudo = [
         {titulo:'olá mundo!(0)', texto:'fbklfglksfysbikgsbkfbsçfbyvlsvvsuo~sviçbuvrsbçs,'},
         {titulo:'olá mundo!(1)', texto:'fbklfglksfysbikgsbkfbsçfbyvlsvvsuo~sviçbuvrsbçs,'},
         {titulo:'olá mundo!(2)', texto:'fbklfglksfysbikgsbkfbsçfbyvlsvvsuo~sviçbuvrsbçs,'},
-        {titulo:'olá mundo!(3)', texto:'fbklfglksfysbikgsbkfbsçfbyvlsvvsuo~sviçbuvrsbçs,'}
-    ]
-
-    console.log(conteudo)
-//})
+        {titulo:'olá mundo!(3)', texto:'fbklfglksfysbikgsbkfbsçfbyvlsvvsuo~sviçbuvrsbçs,'},
+        {titulo:'olá mundo!(4)', texto:'fbklfglksfysbikgsbkfbsçfbyvlsvvsuo~sviçbuvrsbçs,'}
+    ];
+    for(let i = 0; i < conteudo.length; i++){
+        $('div.itens').children().eq(i).hover(function(){
+            $('h4.header').html(conteudo[i].titulo)
+        })
+    }
+    
+})
