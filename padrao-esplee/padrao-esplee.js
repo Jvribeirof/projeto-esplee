c2c3edc62ce0c7cdfa022e7c7cf179efadbdd967
Jvribeirof <107144992+Jvribeirof@users.noreply.github.com>
2022-08-20 20:44:42 -0300

    att

--- padrao-esplee/padrao-esplee.js
+++ padrao-esplee/padrao-esplee.js
@@ -9,4 +9,24 @@ $(document).ready(function(){
     })
 
     //----------------
+
+        //FUNÇÃO DA ANIMAÇÃO
+        function fadeScroll(select){
+            let scrlTop = $(document).scrollTop();
+            let offset = $(window).height() * 3/4
+            let itemTop = select.offset().top;
+
+            if(scrlTop >= itemTop - offset){
+                select.addClass('fade')}
+        }
+            /*
+            //RODA AO DER RE-LOAD NA PÁGINA
+            fadeScroll()
+
+            //RODA AO SCROLLAR A PÁGINA
+            $(document).scroll(function(){
+                fadeScroll();
+            })
+            */
+        
 })
