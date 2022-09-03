17ffb2eebff9cf24fb266289e276bd0d3d00d7d5
Jvribeirof <107144992+Jvribeirof@users.noreply.github.com>
2022-09-02 21:23:07 -0300

    att
    add navbar no contato e criando começando modelo de card em projetos

--- pags/secundarias/contato/contato.js
+++ pags/secundarias/contato/contato.js
@@ -7,27 +7,16 @@ function fadeScroll(select){
         select.addClass('fade')}
 }
 
-//RODA AO DER RE-LOAD NA PÁGINA
-$('.js-pic').each(function(){
-    fadeScroll($(this));
-})
-
-//RODA AO SCROLLAR A PÁGINA
-$(document).scroll(function(){
-    $('.js-pic').each(function(){
-        fadeScroll($(this));
-    })
-})
-
 /*NAVBAR PARA MOBILE*/
 $('#nav-menu-ico, .background, .navbar-icon').click(function(){
+    let img = document.querySelector('#img-cancel');
     $('.navbar').toggleClass('show');
-    $('.background, #nav-menu-ico').fadeToggle(1000);
+    $('.background').fadeToggle(1000);
     $('#nav-menu-ico').toggleClass('icon-fixed')
 
     if($('.navbar').hasClass('show') == true){
-        $('.links a').fadeToggle(900);
+        $('.links a, #nav-menu-ico').fadeToggle(900);
     }else{
-        $('.links a').fadeToggle(400);
+        $('.links a, #nav-menu-ico').fadeToggle(400);
     }
 })
