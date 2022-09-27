269e93a24c6f1128c1b6c09eb8b877d604e7a5f5
Jvribeirof <107144992+Jvribeirof@users.noreply.github.com>
2022-09-27 20:38:16 -0300

    novo index

--- pags/index/index.js
+++ pags/index/index.js
@@ -1,15 +1,20 @@
-$(document).ready(function(){
-    const conteudo = [
-        {titulo:'olá mundo!(0)', texto:'fbklfglksfysbikgsbkfbsçfbyvlsvvsuo~sviçbuvrsbçs,'},
-        {titulo:'olá mundo!(1)', texto:'fbklfglksfysbikgsbkfbsçfbyvlsvvsuo~sviçbuvrsbçs,'},
-        {titulo:'olá mundo!(2)', texto:'fbklfglksfysbikgsbkfbsçfbyvlsvvsuo~sviçbuvrsbçs,'},
-        {titulo:'olá mundo!(3)', texto:'fbklfglksfysbikgsbkfbsçfbyvlsvvsuo~sviçbuvrsbçs,'},
-        {titulo:'olá mundo!(4)', texto:'fbklfglksfysbikgsbkfbsçfbyvlsvvsuo~sviçbuvrsbçs,'}
-    ];
-    for(let i = 0; i < conteudo.length; i++){
-        $('div.itens').children().eq(i).hover(function(){
-            $('h4.header').html(conteudo[i].titulo)
-        })
-    }
-    
+function fadeScroll(select){
+    let scrlTop = $(document).scrollTop();
+    let offset = $(window).height() * 3/4
+    let itemTop = select.offset().top;
+
+    if(scrlTop >= itemTop - offset){
+        select.addClass('fade')}
+}
+
+//RODA AO DER RE-LOAD NA PÁGINA
+$('.js-fade').each(function(){
+    fadeScroll($(this));
+})
+
+//RODA AO SCROLLAR A PÁGINA
+$(document).scroll(function(){
+    $('.js-fade').each(function(){
+        fadeScroll($(this));
+    })
 })
