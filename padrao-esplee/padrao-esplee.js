b3293d27008e27a5058137c2b963880a992f645c
Jvribeirof <107144992+Jvribeirof@users.noreply.github.com>
2022-09-30 20:13:06 -0300

    att index

--- padrao-esplee/padrao-esplee.js
+++ padrao-esplee/padrao-esplee.js
@@ -2,7 +2,7 @@ $(document).ready(function(){
 
     //JS DO ACCORDION
 
-    $('div.accordion .content').hide()
+    $('.content-ac').hide()
     $('div.accordion button').click(function(){
         $(this).children('img').toggleClass('rotate');
         $(this).parent().next().slideToggle(400);
